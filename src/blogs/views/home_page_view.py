2a93afcc417b12d589d1988dd6f23cd474a41b05
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-12-18 19:42:16 +0100

    Simple home page list view

--- src/blogs/views/home_page_view.py
+++ src/blogs/views/home_page_view.py
@@ -1,6 +1,14 @@
-from django.views.generic import TemplateView
+from django.utils import timezone
+from django.views.generic import ListView
+
+from blogs.models import Post
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
 
+    model = Post
     template_name = 'home.html'
+
+    def get_queryset(self):
+        queryset = super(HomePageView, self).get_queryset()
+        return queryset.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
