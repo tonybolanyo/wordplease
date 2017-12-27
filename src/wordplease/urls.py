16bf03307c94db4f0856b50aa3d887ab2582dff8
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-12-27 10:41:32 +0100

    Fix URL config error

--- src/wordplease/urls.py
+++ src/wordplease/urls.py
@@ -33,4 +33,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
