c137dfb2388d707992829f0998f0a4fd6a214897
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-12-28 14:48:30 +0100

    More form styling

--- src/users/forms.py
+++ src/users/forms.py
@@ -11,8 +11,11 @@ class LoginForm(forms.Form):
 
 class SignupForm(UserCreationForm):
 
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    error_css_class = 'form-error'
+    required_css_class = 'form-required'
+
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
