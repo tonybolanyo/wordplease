41a1c201f7587eb466107eccbe8124229c24bb9a
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-01-13 21:10:45 +0100

    Settings fot HTTPS inproduction added

--- src/wordplease/settings_production.py
+++ src/wordplease/settings_production.py
@@ -26,3 +26,11 @@ STATIC_URL = os.environ.get('STATIC_URL', STATIC_URL)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, os.environ.get('MEDIA_ROOT', "media/"))
 MEDIA_URL = os.environ.get('MEDIA_URL', "/media/")
+
+# Para usar con HTTPS
+# Forzamos la redirección HTTP a HTTPS, aunque esto ya lo hacemos con nginx (o deberíamos)
+SECURE_SSL_REDIRECT = True
+# Encriptamos la cookie de sesión (sólo se envía por HTTPS)
+SESSION_COOKIE_SECURE = True
+# Encriptamos la cookie del CSRF (sólo se envía por HTTPS)
+CSRF_COOKIE_SECURE = True
