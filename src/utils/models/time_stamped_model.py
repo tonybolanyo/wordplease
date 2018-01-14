c92adc44b38c4bf2e751717afc773f571b667b05
Tony G. Bolaño <tonybolanyo@gmail.com>
2018-01-14 19:46:34 +0100

    Model TimeStampedModel fields translated to Spanish

--- src/utils/models/time_stamped_model.py
+++ src/utils/models/time_stamped_model.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class TimeStampedModel(models.Model):
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
 
     class Meta:
         abstract = True
